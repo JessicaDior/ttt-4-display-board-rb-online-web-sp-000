944ed7171bd3dfe3786762a759fb5fdd3a0f3063
Jessica Dior <jessicaclairdior@gmail.com>
2020-08-07 01:37:00 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 display_board = [" ", " ", " ", " ", " " , " ", " ", " ", " "]
 
 def display_board (display_board)
-  puts "#{display_board[0]}  | #{display_board[1]} | #{display_board[2]} "
+  puts " #{display_board[0]}  | #{display_board[1]} | #{display_board[2]} "
   puts "-----------"
-  puts "#{display_board[3]}  | #{display_board[4]} |  #{display_board[5]}"
+  puts " #{display_board[3]}  | #{display_board[4]} |  #{display_board[5]}"
   puts "-----------"
-  puts "#{display_board[6]}  | #{display_board[7]} |  #{display_board[8]}"
+  puts " #{display_board[6]}  | #{display_board[7]} |  #{display_board[8]}"
 end
