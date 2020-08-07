5ca2d2ecc51a502a59877cbc3a800f6ae1e3c76b
Jessica Dior <jessicaclairdior@gmail.com>
2020-08-07 01:24:44 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 display_board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-def display_board
+def display_board (display_board)
   puts "#{display_board[0]}, |, #{display_board[1]}, |, #{display_board[2]} "
   puts "-----------"
   puts "#{display_board[3]}, |, #{display_board[4]}, |, #{display_board[5]}"
