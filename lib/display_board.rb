2598dd4e14991cce5462e6ac61da33da07db01e9
Jessica Dior <jessicaclairdior@gmail.com>
2020-08-07 01:24:47 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,4 +6,4 @@ def display_board (display_board)
   puts "#{display_board[3]}, |, #{display_board[4]}, |, #{display_board[5]}"
   puts "-----------"
   puts "#{display_board[6]}, |, #{display_board[7]}, |, #{display_board[8]}"
-end 
+end
